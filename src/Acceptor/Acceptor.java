face8adb177aa1f152adabc3dfc5bd0d04af29a2
Cesar Segura <cesars5773@gmail.com>
2023-11-30 05:48:20 -0500

    Added n select multiplexing loops to the structure of the http server. Now, the server supports n loops (corresponding to n threads) that each can accept new client-server connections and handle requests on them in a non-blocking fashion (however, a mutex lock is used when handling new connections in Accept so that the same connection isn't accepted twice)

--- src/Acceptor/Acceptor.java
+++ src/Acceptor/Acceptor.java
@@ -1,6 +1,7 @@
 package Acceptor;
 
 import java.nio.channels.*;
+import java.util.concurrent.locks.Lock;
 import java.io.IOException;
 import AcceptHandler.AcceptHandler;
 import ApacheConfig.ApacheConfig;
@@ -12,10 +13,12 @@ public class Acceptor implements AcceptHandler {
     private SocketReadWriteHandlerFactory srwf;
     private String CGI_BIN;
     private ApacheConfig config;
-    public Acceptor(SocketReadWriteHandlerFactory srwf, ApacheConfig config, String CGI_BIN) {
+    private Lock acceptLock; 
+    public Acceptor(SocketReadWriteHandlerFactory srwf, ApacheConfig config, String CGI_BIN, Lock acceptLock) {
         this.srwf = srwf;
         this.config = config;
         this.CGI_BIN = CGI_BIN;
+        this.acceptLock = acceptLock;
     }
 
     public void handleException() {
@@ -23,27 +26,24 @@ public class Acceptor implements AcceptHandler {
     }
 
     public void handleAccept(SelectionKey key) throws IOException {
-        ServerSocketChannel server = (ServerSocketChannel) key.channel();
-
-        // extract the ready connection
-        SocketChannel client = server.accept();
-        DEBUG("handleAccept: Accepted connection from " + client);
-
-        // configure the connection to be non-blocking
-        client.configureBlocking(false);
-
-        /*
-         * register the new connection with *read* events/operations
-         * SelectionKey clientKey = client.register( selector,
-         * SelectionKey.OP_READ);// | SelectionKey.OP_WRITE);
-         */
-
-        ReadWriteHandler rwH = srwf.createHandler(config, CGI_BIN);
-        int ops = rwH.getInitOps();
-
-        SelectionKey clientKey = client.register(key.selector(), ops);
-        clientKey.attach(rwH);
-
+        try {
+            acceptLock.lock();
+            ServerSocketChannel server = (ServerSocketChannel) key.channel();
+            // extract the ready connection
+            SocketChannel client = server.accept();
+            if (client != null) {
+                DEBUG("handleAccept: Accepted connection from " + client);
+                client.configureBlocking(false); 
+                ReadWriteHandler rwH = srwf.createHandler(config, CGI_BIN);
+                int ops = rwH.getInitOps();
+                SelectionKey clientKey = client.register(key.selector(), ops);
+                clientKey.attach(rwH); 
+            } else {
+                DEBUG("handleAccept: client was null, no connection accepted");
+            }
+        } finally {
+            acceptLock.unlock();
+        }
     } // end of handleAccept
     private static void DEBUG(String s) {
         System.out.println(s);
